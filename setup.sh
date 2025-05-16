59a5dae11f62ff4a01f553da574d989cd2fd2782
dendogg <densmaltz4@gmail.com>
2025-05-16 05:05:29 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create necessary folders
+mkdir -p assets
+mkdir -p css
+mkdir -p js
+mkdir -p data
+
+echo "Created project directory structure!"
+echo "Add your banner image to the assets folder for the README."
+echo ""
+echo "To start the Spatial Directory Explorer, run:"
+echo "chmod +x run_spatial_explorer.sh"
+echo "./run_spatial_explorer.sh [optional-directory-path]"
